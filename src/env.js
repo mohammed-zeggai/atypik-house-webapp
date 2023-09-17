4d8ac6f656414a08ef0904672ed901970a6370a8
Mohammed ZEGGAI <59542121+mohammed-zeggai@users.noreply.github.com>
2023-09-17 18:55:01 +0200

    Update env.js

--- src/env.js
+++ src/env.js
@@ -1,8 +1,8 @@
 
 export const apiUrl = window.location.origin.includes('localhost')
   ? 'http://localhost:8080'
-  : 'http://159.223.2.119:8080';
+  : 'https://atypik-house-api-y8ntq.ondigitalocean.app';
 
 export const webUrl = window.location.origin.includes('localhost')
   ? 'http://localhost:8081'
-  : 'http://159.223.2.119';
+  : 'https://atypik-house-api-y8ntq.ondigitalocean.app';
