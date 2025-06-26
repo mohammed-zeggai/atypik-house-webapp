a68a53caf9e5ab79cbd9df5681471fe8e6ead8e0
Mohammed <mohammedzeggai9@gmail.com>
2025-06-27 00:41:18 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -15,20 +15,7 @@ export default ({ command, ssrBuild }) => {
         noExternal: ['vue-meta']
       },
       server: {
-        host: '0.0.0.0',
-        port: 8080,
-        configureServer: (server) => {
-          server.middlewares.use((req, res, next) => {
-            const allowedHosts = ['https://atypik-house-ssr-web-app-vyxof.ondigitalocean.app', 'https://f2i-dev06-mz-ak-yy-ae.fr', 'http://localhost']
-            const hostHeader = req.headers.host?.split(':')[0]
-            if (!allowedHosts.includes(hostHeader || '')) {
-              res.statusCode = 403
-              res.end('Forbidden')
-              return
-            }
-            next()
-          })
-        }
+        allowedHosts: 'all'
       }
     }
   } else {
