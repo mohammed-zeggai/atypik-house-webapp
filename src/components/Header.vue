b41dcf58d26a208cd24412a2546937ab0d712d3c
Mohammed <mohammedzeggai9@gmail.com>
2025-07-11 22:26:04 +0200

    header changes

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,23 @@
 <template>
-  <nav class="navbar fixed-top navbar-expand-lg scrolling-navbar">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="/">
-        <img :src="`/logo.png`" alt="Logo" id="logo" />
-        </a>
+  <nav class="navbar navbar-expand-lg fixed-top navbar-dark px-3">
+    <div class="container-fluid p-0 d-flex align-items-center justify-content-center" style="gap: 3rem;">
+  <!-- Logo -->
+  <a class="navbar-brand m-0" href="/" style="display: flex; align-items: center;">
+    <img :src="`/logo.png`" alt="Logo" style="height: 55px; object-fit: contain;" />
+  </a>
 
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+  <!-- Bouton Hamburger -->
+  <button
+    class="navbar-toggler"
+    type="button"
+    data-bs-toggle="collapse"
+    data-bs-target="#navbarNavDropdown"
+    aria-controls="navbarNavDropdown"
+    aria-expanded="false"
+    aria-label="Toggle navigation"
+  >
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
       <div class="collapse navbar-collapse"  id="navbarNavDropdown">
         <ul class="navbar-nav">
